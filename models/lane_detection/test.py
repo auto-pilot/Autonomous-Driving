76982949069b89602cc7dc7b0438e5786556b487
namig <namiq.aliyev.871@gmail.com>
2020-07-12 03:07:34 +0300

    test script changing, best model adding

--- models/lane_detection/test.py
+++ models/lane_detection/test.py
@@ -1,15 +1,91 @@
-import cv2
 import numpy as np
-import os
+import cv2
 import keras
 from keras.models import load_model
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+import time
+import spidev
+import pigpio
+
+# pinout
+in3 = 22
+in4 = 24
+ena = 23
+
+# gpio configuration
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(ena,GPIO.OUT)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
+q=GPIO.PWM(ena,1000)
+q.start(25)
+
+pi = pigpio.pi()
+pi.set_mode(17, pigpio.OUTPUT)
+
+
+def to_angle(x):
+    return 600 + (1400 / 255) * x #0-> 600 255->2000
+
+def to_speed(x):
+    speed = 0 + (100 / 127) * (x - 128)
+    speed = abs(speed)
+    if speed < 0:
+        return 0
+    elif speed > 100:
+        return 100
+    else:
+        return speed
+
 
 if __name__ == '__main__':
-    model = load_model('8-13-59-3.h5')
-
-    img = cv2.imread('dataset/1/image424.jpg')
-    img = cv2.Canny(img, 20, 50)
-    img = cv2.resize(img, (128,128))
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    print(img.shape)
-    print(np.argmax(model.predict(img.reshape(1,128,128,3))))
+    model = load_model('26-20-39-50.h5')
+    
+    # resizing video
+    cap = cv2.VideoCapture(0);
+    cap.set(3, 480)
+    cap.set(4, 480)
+    
+    while True:
+        start_time = time.time()
+        
+        ret, frame = cap.read()
+	
+	# taking edges from frame
+        frame = cv2.flip(frame, -1)
+        frame = cv2.Canny(frame, 20, 50)
+        frame = cv2.resize(frame, (128,128))
+        frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+	# model prediction
+        estimate = np.argmax(model.predict(frame.reshape(1, 128, 128, 3)))
+        
+	
+        print(estimate)
+        if estimate == 0:
+            pi.set_servo_pulsewidth(17, 1500)
+            GPIO.output(in3,GPIO.LOW)
+            GPIO.output(in4,GPIO.HIGH)
+            q.ChangeDutyCycle(45)
+            time.sleep(0.1)
+            GPIO.output(in3,GPIO.LOW)
+            GPIO.output(in4,GPIO.LOW)
+        elif estimate == 1:
+            pi.set_servo_pulsewidth(17, 1045)
+            GPIO.output(in3,GPIO.LOW)
+            GPIO.output(in4,GPIO.HIGH)
+            q.ChangeDutyCycle(65)
+            time.sleep(0.125)
+            GPIO.output(in3,GPIO.LOW)
+            GPIO.output(in4,GPIO.LOW)
+        elif estimate == 2:
+            pi.set_servo_pulsewidth(17, 900)
+            GPIO.output(in3,GPIO.LOW)
+            GPIO.output(in4,GPIO.HIGH)
+            q.ChangeDutyCycle(80)
+            time.sleep(0.125)
+            GPIO.output(in3,GPIO.LOW)
+            GPIO.output(in4,GPIO.LOW)
+        
+        print("FPS: ", 1.0 / (time.time() - start_time))
